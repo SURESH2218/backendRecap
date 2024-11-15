a08ef599cf39741bbc9862ea83f5ae329311e437
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-11-15 15:49:32 +0530

    d

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,1 +1,22 @@
+import mongoose from "mongoose";
 
+export const dbConnect = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://sureshalabani2785:Suresh%4022@cluster0.sghfo.mongodb.net/todo_app?retryWrites=true&w=majority"
+    );
+    console.log("connected successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const todoSchema = mongoose.Schema({
+  title: "String",
+  description: "String",
+  completed: Boolean,
+});
+
+const todo = mongoose.model("todos", todoSchema);
+
+export default todo;
--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,63 @@
 import express from "express";
-import dbConnect from "./db/index.js";
-
+import { createTodo } from "./types.js";
+import todo, { dbConnect } from "./db/index.js";
 const app = express();
 
 app.use(express.json());
+
+app.post("/todo", async (req, res) => {
+  const createPayload = req.body;
+  const parsedPayload = createTodo.safeParse(createPayload);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "You sent the wrong inputs",
+    });
+    return;
+  }
+  await todo.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false,
+  });
+
+  res.json({
+    msg: "todo created",
+  });
+});
+
+app.get("/", (req, res) => {
+  req.send("server running");
+});
+
+app.get("/todos", async (req, res) => {
+  const todos = await todo.find({});
+  //   console.log(todos);
+  res.json({
+    todos,
+  });
+});
+
+app.put("/completed", async (req, res) => {
+  const createPayload = req.body;
+  const parsedPayload = createTodo.safeParse(createPayload);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "You sent the wrong inputs",
+    });
+    return;
+  }
+  await todo.update(
+    {
+      _id: req.body.id,
+    },
+    {
+      completed: true,
+    }
+  );
+  res.json({
+    msg: "updated successfully",
+  });
+});
+
 dbConnect();
-app.post("/todo", (req, res) => {
-
-});
-app.get("/todos", (req, res) => {
-
-});
-app.put("/completed", (req, res) => {
-    
-});
-
 app.listen(3000);
